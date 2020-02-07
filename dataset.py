1cb8bb7d92f165ffee7d52d6bd5f63e602bbe8fd
XiaoSanGit <849304059@qq.com>
2020-02-07 21:16:39 +0800

    2/7 start VAE coding by pytorch lightning

---
+++ dataset.py
@@ -0,0 +1,75 @@
+import math
+import os
+import random
+
+import cv2
+import numpy as np
+
+from torch.utils.data.dataset import Dataset
+
+class SkeletonTrainDataset(Dataset):
+    #TODO whether we should use heatmaps? or directly turn to skeleton image
+    # if use heatmaps, i propose to use .npy format to save the pretrained data
+    def __init__(self, dataset_folder, stride, sigma, transform=None):
+        super().__init__()
+        self._dataset_folder = dataset_folder
+        # self._stride = stride
+        # self._sigma = sigma
+        self._transform = transform
+        self.train_labels = [line.rstrip('\n') for line in
+                        open(os.path.join(self._dataset_folder, 'Train', 'index.csv'), 'r')] #save the path of image/npy pair + conditional image
+
+    def __getitem__(self, idx):
+        tokens = self.train_labels[idx].split(',') #suggest format [input_real_npy_path, input_condition_path, output_anime_npy_path]
+        real_skeletion = np.load(os.path.join(self._dataset_folder, 'Train', 'inputs', tokens[0]))
+        condition_img = cv2.imread(os.path.join(self._dataset_folder, 'Train', 'conditions', tokens[1]), cv2.IMREAD_COLOR)
+        anime_skeletion = np.load(os.path.join(self._dataset_folder, 'Train', 'gt', tokens[2]))
+        #TODO maybe resize the inputs
+        sample = {
+            'real': real_skeletion,
+            'condition': condition_img,
+            'anime':anime_skeletion
+        }
+        if self._transform:
+            sample = self._transform(sample) #TODO NOT SURE
+        # normization
+        image = sample['condition'].astype(np.float32)
+        image = (image - 128) / 256 #turn to -0.5~0.5
+        sample['condition'] = image.transpose((2, 0, 1)) #C,W,H
+        return sample
+
+    def __len__(self):
+        return len(self.train_labels)
+
+
+
+class SkeletonValDataset(Dataset):
+    def __init__(self, dataset_folder, num_images=-1):
+        # input should be heatmaps or skeleton image, output should be also heatmaps or skeleton image
+        super().__init__()
+        self._dataset_folder = dataset_folder
+        self.index_path = os.path.join(self._dataset_folder, 'Val', 'index.csv')
+        self.val_labels = [line.rstrip('\n') for line in open(self.index_path, 'r')]
+        if num_images > 0:
+            self.val_labels = self.val_labels[:num_images]
+
+    def __getitem__(self, idx):
+        tokens = self.val_labels[idx].split(',')  # suggest format [input_real_npy_path, input_condition_path, output_anime_npy_path]
+        real_skeletion = np.load(os.path.join(self._dataset_folder, 'Val', 'inputs', tokens[0]))
+        condition_img = cv2.imread(os.path.join(self._dataset_folder, 'Val', 'conditions', tokens[1]),
+                                   cv2.IMREAD_COLOR)
+        anime_skeletion = np.load(os.path.join(self._dataset_folder, 'Val', 'gt', tokens[2]))
+        # TODO maybe resize the inputs
+        sample = {
+            'real': real_skeletion,
+            'condition': condition_img,
+            'anime': anime_skeletion
+        }
+        # normization
+        # image = sample['condition'].astype(np.float32)
+        # image = (image - 128) / 256  # turn to -0.5~0.5
+        # sample['condition'] = image.transpose((2, 0, 1))  # C,W,H
+        return sample
+
+    def __len__(self):
+        return len(self.val_labels)
--- model.py
+++ model.py
@@ -2,11 +2,125 @@ import os, sys
 sys.path.append(os.getcwd())
 from settings import args
 import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+import torch
 
+import pytorch_lightning as ptl
+import logging as log
+from argparse import ArgumentParser
+from collections import OrderedDict
+from torch.utils.data.distributed import DistributedSampler
+from torchvision.datasets import MNIST
 
+from dataset import SkeletonTrainDataset,SkeletonValDataset
+# ==================Definition Start - Stage 1======================
+class skeletonVAE(ptl.LightningModule):
+    def __init__(self):
+        super(skeletonVAE, self).__init__()
+        # not the best model...
+        #TODO construct model structure
+        self.l1 = torch.nn.Linear(28 * 28, 10)
 
+    def forward(self, x, c):#input, condition
+        # TODO construct forward
+        # here is flatten for c,w*h, then conv, relu
+        return torch.relu(self.l1(x.view(x.size(0), -1)))
 
-# ==================Definition Start======================
+    def my_loss(self, y_hat, y):
+        # TODO construct loss, y_hat is the predicted
+        return F.cross_entropy(y_hat, y)
+
+    def training_step(self, batch, batch_nb):
+        x, c, y = batch
+        y_hat = self.forward(x,c)
+        return {'loss': self.my_loss(y_hat, y)}
+
+    def validation_step(self, batch, batch_nb):
+        x, c, y = batch
+        y_hat = self.forward(x, c)
+        return {'val_loss': self.my_loss(y_hat, y)}
+
+    def validation_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        return {'avg_val_loss': avg_loss}
+
+    def configure_optimizers(self):
+        #TODO if we want to finetune, add para.requires_grad = False in the separent part of model
+        return [torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=0.02)]
+
+    def __dataloader(self, train):
+        # init data generators
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5,), (1.0,))])
+        #TODO construct datasets
+        dataset = SkeletonTrainDataset if train else SkeletonValDataset
+
+        # when using multi-node (ddp) we need to add the  datasampler
+        train_sampler = None
+        batch_size = self.hparams.batch_size
+
+        if self.use_ddp:
+            train_sampler = DistributedSampler(dataset)
+
+        should_shuffle = train_sampler is None
+        loader = DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=should_shuffle,
+            sampler=train_sampler,
+            num_workers=0
+        )
+
+        return loader
+
+    @ptl.data_loader
+    def train_dataloader(self):
+        log.info('Training data loader called.')
+        return self.__dataloader(train=True)
+
+    @ptl.data_loader
+    def val_dataloader(self):
+        log.info('Validation data loader called.')
+        return self.__dataloader(train=False)
+
+    @ptl.data_loader
+    def test_dataloader(self):
+        log.info('Test data loader called.')
+        return self.__dataloader(train=False)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser, root_dir):  # pragma: no cover
+        """
+        Parameters you define here will be available to your model through self.hparams
+        :param parent_parser:
+        :param root_dir:
+        :return:
+        """
+        parser = ArgumentParser(parents=[parent_parser])
+
+        # param overwrites
+        # parser.set_defaults(gradient_clip_val=5.0)
+
+        # network params
+        parser.add_argument('--in_features', default=28 * 28, type=int)
+        parser.add_argument('--out_features', default=10, type=int)
+        # use 500 for CPU, 50000 for GPU to see speed difference
+        parser.add_argument('--hidden_dim', default=50000, type=int)
+        parser.add_argument('--drop_prob', default=0.2, type=float)
+        parser.add_argument('--learning_rate', default=0.001, type=float)
+
+        # data
+        parser.add_argument('--data_root', default=os.path.join(root_dir, 'datasets'), type=str)
+
+        # training params (opt)
+        parser.add_argument('--optimizer_name', default='adam', type=str)
+        parser.add_argument('--batch_size', default=32, type=int)
+        return parser
+
+# ==================Definition Start - Stage 12======================
 class Generator(nn.Module):
     def __init__(self):
         #TODO change model
@@ -78,7 +192,4 @@ class Discriminator(nn.Module):
         out = out.view(-1, 4*4*4*args.dim)
         out = self.output(out)
         return out.view(-1)
-
-
-
 
